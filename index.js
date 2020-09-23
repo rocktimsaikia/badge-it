c2eb71736d710661e0e8443f9f2e06da18148230
rocktimsaikia <srocktim61@gmail.com>
2020-09-23 18:16:27 +0530

    test with logs 6

--- index.js
+++ index.js
@@ -74,6 +74,7 @@ class GenerateBadges {
 		console.log(`header: ${headerMd}`);
 		console.log(`newHeader: ${newHeaderMd}`);
 		const regex = new RegExp(headerMd, 'g');
+		console.log(typeof content);
 		console.log(`updatedReadme T: ${content.replace(regex, newHeaderMd.toString())}`);
 
 		return updatedReadme;
